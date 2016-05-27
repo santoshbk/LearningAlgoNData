b858cb170c51415fe1cf13a6c7fb890284ae7ac3
Santosh K <bornfree.kushi@gmail.com>
2016-05-27 19:41:48 +0530

    Sherlock and squares

---
+++ DataStructuresAndAlgorithms/src/santosh/algorithms/hackerrank/SherlockAndSquares.java
@@ -0,0 +1,28 @@
+package santosh.algorithms.hackerrank;
+
+import java.util.Scanner;
+
+public class SherlockAndSquares {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while (t-- > 0) {
+			long a = in.nextLong();
+			long b = in.nextLong();
+			solution(a, b);
+		}
+		in.close();
+	}
+
+	private static void solution(long a, long b) {
+		int start = (int) Math.ceil(Math.sqrt(a));
+		int end = (int) Math.floor(Math.sqrt(b));
+		int count = 0;
+		for (int i = start; i <= end; i++) {
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
