430b22e5c1c9c50596503829050bee36f3494238
Santosh K <bornfree.kushi@gmail.com>
2016-09-09 17:53:43 +0530

    Refractoring

--- DataStructuresAndAlgorithms/src/santosh/Challenge/domains/java/Datatypes.java
+++ DataStructuresAndAlgorithms/src/santosh/Challenge/domains/java/Datatypes.java
@@ -4,39 +4,46 @@ import java.util.Scanner;
 
 public class Datatypes {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int T = in.nextInt();
-		for (int i = 0; i < T; i++) {
-			try {
-				long n = in.nextLong();
-				System.out.println(n + " can be fitted in:");
-				if (Byte.MIN_VALUE <= n && n <= Byte.MAX_VALUE) {
-					System.out.println("* byte");
-					System.out.println("* short");
-					System.out.println("* int");
-					System.out.println("* long");
-					continue;
-				}
-				if (Short.MIN_VALUE <= n && n <= Short.MAX_VALUE) {
-					System.out.println("* short");
-					System.out.println("* int");
-					System.out.println("* long");
-					continue;
-				}
-				if (Integer.MIN_VALUE <= n && n <= Integer.MAX_VALUE) {
-					System.out.println("* int");
-					System.out.println("* long");
-					continue;
-				}
-				if (Long.MIN_VALUE <= n && n <= Long.MAX_VALUE) {
-					System.out.println("* long");
-					continue;
-				}
-			} catch (Exception e) {
-				System.out.println(in.next() + " can't be fitted anywhere.");
-			}
+    private static final String BYTE = "* byte";
+    private static final String SHORT = "* short";
+    private static final String INT = "* int";
+    private static final String LONG = "* long";
+
+    public static void main(final String[] args) {
+	Scanner in = new Scanner(System.in);
+	int T = in.nextInt();
+	for (int i = 0; i < T; i++) {
+	    try {
+		long n = in.nextLong();
+		System.out.println(n + " can be fitted in:");
+
+		if ((Byte.MIN_VALUE <= n) && (n <= Byte.MAX_VALUE)) {
+		    System.out.println(BYTE);
+		    System.out.println(SHORT);
+		    System.out.println(INT);
+		    System.out.println(LONG);
+		    continue;
 		}
+		if ((Short.MIN_VALUE <= n) && (n <= Short.MAX_VALUE)) {
+		    System.out.println(SHORT);
+		    System.out.println(INT);
+		    System.out.println(LONG);
+		    continue;
+		}
+		if ((Integer.MIN_VALUE <= n) && (n <= Integer.MAX_VALUE)) {
+		    System.out.println(INT);
+		    System.out.println(LONG);
+		    continue;
+		}
+		if ((Long.MIN_VALUE <= n) && (n <= Long.MAX_VALUE)) {
+		    System.out.println(LONG);
+		    continue;
+		}
+	    } catch (Exception e) {
+		System.out.println(in.next() + " can't be fitted anywhere.");
+	    }
 	}
+	in.close();
+    }
 
 }
