685e74118b90d9dc8993bb3a1efa6df3a73331c9
Santosh K <bornfree.kushi@gmail.com>
2016-09-16 20:26:24 +0530

    divisible sum pair

---
+++ DataStructuresAndAlgorithms/src/santosh/algorithms/hackerrank/DivisibleSumPairs.java
@@ -0,0 +1,24 @@
+package santosh.algorithms.hackerrank;
+
+import java.util.Scanner;
+
+public class DivisibleSumPairs {
+    public static void main(final String[] args) {
+	Scanner in = new Scanner(System.in);
+	int n = in.nextInt();
+	int k = in.nextInt();
+	int[] a = new int[n];
+	for (int a_i = 0; a_i < n; a_i++) {
+	    a[a_i] = in.nextInt();
+	}
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+	    for (int j = i + 1; j < n; j++) {
+		if (((a[i] + a[j]) % k) == 0) {
+		    sum++;
+		}
+	    }
+	}
+	System.out.println(sum);
+    }
+}
