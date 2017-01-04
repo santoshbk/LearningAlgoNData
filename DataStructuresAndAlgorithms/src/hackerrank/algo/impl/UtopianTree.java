0538fd9cc8583db5b1789a9109d16c87247dd367
Santosh K <bornfree.kushi@gmail.com>
2017-01-04 14:34:49 +0000

    Test from codenvy

--- DataStructuresAndAlgorithms/src/hackerrank/algo/impl/UtopianTree.java
+++ DataStructuresAndAlgorithms/src/hackerrank/algo/impl/UtopianTree.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class UtopianTree {
 
     public static void main(String[] args) {
+        
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         for(int a0 = 0; a0 < t; a0++){
