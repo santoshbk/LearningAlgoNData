1947ad918758c701c2b7be10eca41fafc87f306f
santro <santro@santosh>
2017-01-24 14:13:30 +0530

    Balanced brackets - solved!

--- DataStructuresAndAlgorithms/src/hackerrank/ds/stacks/BalancedBrackets.java
+++ DataStructuresAndAlgorithms/src/hackerrank/ds/stacks/BalancedBrackets.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class BalancedBrackets {
-	public static void main(final String[] args) {
-		Scanner in = new Scanner(System.in);
+
+	public static boolean isBalanced(final String expression) {
 		Map<Character, Character> m = new HashMap<>();
 		List<Character> list = Arrays.asList('{', '(', '[');
 		m.put('(', ')');
@@ -18,40 +18,37 @@ public class BalancedBrackets {
 		m.put(')', '(');
 		m.put('}', '{');
 		m.put(']', '[');
+		int l = expression.length();
+		if (!list.contains(expression.charAt(0)) || l % 2 != 0 || list.contains(expression.charAt(l - 1))) {
+			return false;
+		}
+		Stack<Character> stack = new Stack<>();
+		stack.push(expression.charAt(0));
+		for (int i = 1; i < l; i++) {
+			char sc = expression.charAt(i);
+			if (!list.contains(sc)) {
+				if (stack.isEmpty()) {
+					return false;
+				}
+				char c = stack.pop();
+				if (expression.charAt(i) != m.get(c)) {
+					return false;
+				}
+			} else {
+				stack.push(sc);
+			}
+		}
+		return true;
+	}
+
+	public static void main(final String[] args) {
+		Scanner in = new Scanner(System.in);
+
 		int t = in.nextInt();
 		in.nextLine();
 		while (t-- > 0) {
-			String s = in.nextLine();
-			int l = s.length();
-			boolean flag = false;
-			if (!list.contains(s.charAt(0)) || l % 2 != 0) {
-				System.out.println("NO");
-				continue;
-			}
-			Stack<Character> stack = new Stack<>();
-			stack.push(s.charAt(0));
-			for (int i = 1; i < l; i++) {
-				char sc = s.charAt(i);
-				if (!list.contains(sc)) {
-					if (stack.isEmpty()) {
-						flag = true;
-						break;
-					}
-					char c = stack.pop();
-					if (s.charAt(i) != m.get(c)) {
-						flag = true;
-						break;
-					}
-				} else {
-					stack.push(sc);
-				}
-			}
-
-			if (!flag) {
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
-			}
+			String s = in.next();
+			System.out.println((isBalanced(s)) ? "YES" : "NO");
 		}
 	}
 }
