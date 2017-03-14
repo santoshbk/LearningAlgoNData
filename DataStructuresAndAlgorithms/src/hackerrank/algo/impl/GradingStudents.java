e6dca455179024776f54f36866e7fabd3c411b94
skumbara062016 <skumbara062016@IN09904033D.dsk.asi.msd.world.socgen>
2017-03-14 15:43:40 +0530

    grading students

--- DataStructuresAndAlgorithms/src/hackerrank/algo/impl/GradingStudents.java
+++ DataStructuresAndAlgorithms/src/hackerrank/algo/impl/GradingStudents.java
@@ -1,9 +1,31 @@
 package hackerrank.algo.impl;
 
+import java.util.Scanner;
+
 public class GradingStudents {
 
-	public static void main(String[] args) {
+	private static int calculateGrade(int i) {
+		if (i < 38) {
+			return i;
+		}
+		int k = i % 5;
+		if (k > 2) {
+			return i + 5 - k;
+		}
 
+		return i;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		int a[] = new int[t];
+		for (int i = 0; i < t; i++) {
+			a[i] = in.nextInt();
+		}
+		for (int i : a) {
+			System.out.println(calculateGrade(i));
+		}
 	}
 
 }
