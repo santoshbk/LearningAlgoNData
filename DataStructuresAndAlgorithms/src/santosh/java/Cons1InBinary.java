631662e86c9765a3b3d852f821cd7b1c4849f1f7
santoshbk <bornfree.kushi@gmail.com>
2017-05-30 15:23:00 +0530

    Create Cons1InBinary.java

---
+++ DataStructuresAndAlgorithms/src/santosh/java/Cons1InBinary.java
@@ -0,0 +1,25 @@
+package com;
+
+import java.util.Scanner;
+
+public class Cons1InBinary {
+
+	public static void main(final String[] args) {
+		Scanner in = new Scanner(System.in);
+		int k = in.nextInt();
+		int count = 0;
+		int maxCount = 0;
+		while (k > 0) {
+			int x = k % 2;
+			if (x == 1) {
+				count++;
+				maxCount = count > maxCount ? count : maxCount;
+			} else {
+				count = 0;
+			}
+			k = k / 2;
+		}
+		System.out.println(maxCount);
+	}
+
+}
