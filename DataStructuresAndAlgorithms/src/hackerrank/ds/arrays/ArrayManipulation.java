afeb8645a4becebf87a3dc55aeb45a8c54436896
Santosh K <bornfree.kushi@gmail.com>
2018-03-01 22:33:50 +0530

    Array manipulation -- in progress

---
+++ DataStructuresAndAlgorithms/src/hackerrank/ds/arrays/ArrayManipulation.java
@@ -0,0 +1,28 @@
+package hackerrank.ds.arrays;
+
+import java.util.Scanner;
+
+public class ArrayManipulation {
+
+	public static void main(final String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] arr = new int[n];
+		int m = in.nextInt();
+		long max = Long.MIN_VALUE;
+		for (int a0 = 0; a0 < m; a0++) {
+			int a = in.nextInt();
+			a--;
+			int b = in.nextInt();
+			b--;
+			int k = in.nextInt();
+			for (int i = a; i < b; i++) {
+				arr[i] += k;
+				int j = arr[i];
+				max = j < max ? max : j;
+			}
+		}
+		System.out.println(max);
+		in.close();
+	}
+}
